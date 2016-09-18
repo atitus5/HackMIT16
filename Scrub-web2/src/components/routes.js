642ea9a615800972fa241bc1186d42e743196c7c
Phil Cherner <pcherner@mit.edu>
2016-09-18 10:40:37 -0400

    fixed reference errorl

--- Scrub-web2/src/components/routes.js
+++ Scrub-web2/src/components/routes.js
@@ -5,12 +5,10 @@ import { Route, IndexRedirect } from 'react-router';
 
 import App from './Main';
 import SearchComponent from './SearchComponent.js';
-import LinkComponent from './LinkComponent.js';
 
 module.exports = (
     <Route path="/" component={App}>
         <IndexRedirect to="/search" />
         <Route path='search' component={SearchComponent}/>
-        <Route path='links' component={LinkComponent} />
     </Route>
 )
